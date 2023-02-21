d37d22edeec816dd68ad828f3fb4ff73a79e34d8
fiz232 <fionalim1997@hotmail.com>
2023-02-21 12:26:24 +0800

    updated nav bar

--- mern-frontend/src/App.js
+++ mern-frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import Main from "./components/main";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // components: header, main
 
--- mern-frontend/src/components/header.js
+++ mern-frontend/src/components/header.js
@@ -1,24 +1,30 @@
-import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function Header() {
   return (
     <div>
-      <h1>Learn origami in simple steps!</h1>
-      <nav>
-        <Link to="/">
-          <div>Home</div>
-        </Link>
-        <Link to="/about">
-          <div>About</div>
-        </Link>
-        <Link to="/login">
-          <div>Login</div>
-        </Link>
-        <Link to="/register">
-          <div>Register</div>
-        </Link>
-        <p>You are not logged in</p>
-      </nav>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src="/logo.png"
+              width="150px"
+              height="110px"
+              className="d-inline-block align-top"
+              alt="OrigaMe logo"
+            />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      ;
     </div>
   );
 }
